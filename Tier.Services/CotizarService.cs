3f835f5faa12606ecc0a2af56830c9ecbb9a69ae
r-next <r@next-software.co>
2015-08-18 21:37:50 -0500

    Se agrega el servicio nuevamente a la solucion.

---
+++ Tier.Services/CotizarService.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tier.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CotizarService" in both code and config file together.
+    public class CotizarService : ICotizarService
+    {
+        #region [Gestión Empresas]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Empresa> Empresa_RecuperarFiltrado(Dto.Empresa objFiltros)
+        {
+            return new Business.BEmpresa().RecuperarFiltrado(objFiltros);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idEmpresa"></param>
+        /// <returns></returns>
+        public bool Empresa_Insertar(Dto.Empresa obj, out byte? idEmpresa)
+        {
+            bool blnRespuesta = new Business.BEmpresa().Crear(obj);
+
+            if (blnRespuesta)
+                idEmpresa = obj.idempresa;
+            else
+                idEmpresa = null;
+
+            return blnRespuesta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Empresa_Actualizar(Dto.Empresa obj)
+        {
+            return new Business.BEmpresa().Actualizar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Empresa_Eliminar(Dto.Empresa obj)
+        {
+            return new Business.BEmpresa().Eliminar(obj);
+        }
+        #endregion
+
+        #region [Gestión Roles]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Rol> Rol_RecuperarFiltros(Dto.Rol objFiltros)
+        {
+            return new Business.BRol().RecuperarFiltrado(objFiltros);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idrol"></param>
+        /// <returns></returns>
+        public bool Rol_Insertar(Dto.Rol obj, short? idrol)
+        {
+            bool blnRespuesta = new Business.BRol().Crear(obj);
+
+            if (blnRespuesta)
+                idrol = obj.idrol;
+            else
+                idrol = null;
+
+            return blnRespuesta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Rol_Actualizar(Dto.Rol obj)
+        {
+            return new Business.BRol().Actualizar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Rol_Eliminar(Dto.Rol obj)
+        {
+            return new Business.BRol().Eliminar(obj);
+        }
+        #endregion
+
+        #region [Gestión Listas]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.ItemLista> ItemLista_RecuperarFiltros(Dto.ItemLista objFiltros)
+        {
+            return new Business.BItemsLista().RecuperarFiltrado(objFiltros);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="iditemlista"></param>
+        /// <returns></returns>
+        public bool ItemLista_Insertar(Dto.ItemLista obj, out int? iditemlista)
+        {
+            bool blnRespuesta = new Business.BItemsLista().Crear(obj);
+
+            if (blnRespuesta)
+                iditemlista = obj.iditemlista;
+            else
+                iditemlista = null;
+
+            return blnRespuesta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool ItemLista_Actualizar(Dto.ItemLista obj)
+        {
+            return new Business.BItemsLista().Actualizar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool ItemLista_Eliminar(Dto.ItemLista obj)
+        {
+            return new Business.BItemsLista().Eliminar(obj);
+        }
+
+        #endregion
+
+        #region [Gestión Usuarios]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Usuario> Usuario_RecuperarFiltros(Dto.Usuario objFiltros)
+        {
+            return new Business.BUsuario().RecuperarFiltrado(objFiltros);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idusuario"></param>
+        /// <returns></returns>
+        public bool Usuario_Insertar(Dto.Usuario obj, short? idusuario)
+        {
+            bool blnRespuesta = new Business.BUsuario().Crear(obj);
+
+            if (blnRespuesta)
+                idusuario = obj.idusuario;
+            else
+                idusuario = null;
+
+            return blnRespuesta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Usuario_Actualizar(Dto.Usuario obj)
+        {
+            return new Business.BUsuario().Actualizar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Usuario_Eliminar(Dto.Usuario obj)
+        {
+            return new Business.BUsuario().Eliminar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Usuario_RestablecerClave(Dto.Usuario obj)
+        {
+            return new Business.BUsuario().RestablecerClave(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Usuario_CambiarClave(Dto.Usuario obj)
+        {
+            return new Business.BUsuario().CambiarClave(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Usuario_ValidaNombreUsuario(Dto.Usuario obj)
+        {
+            return new Business.BUsuario().ValidaNombreUsuario(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public Dto.Sesion Usuario_IniciarSesion(Dto.Usuario obj)
+        {
+            return new Business.BUsuario().IniciarSesion(obj);
+        }
+        #endregion
+
+        #region [Gestión Maquinas]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Maquina> Maquina_RecuperarFiltros(Dto.Maquina objFiltros)
+        {
+            return new Business.BMaquina().RecuperarFiltrado(objFiltros);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idmaquina"></param>
+        /// <returns></returns>
+        public bool Maquina_Insertar(Dto.Maquina obj, short? idmaquina)
+        {
+            bool blnRespuesta = new Business.BMaquina().Crear(obj);
+
+            if (blnRespuesta)
+                idmaquina = obj.idmaquina;
+            else
+                idmaquina = null;
+
+            return blnRespuesta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Maquina_Actualizar(Dto.Maquina obj)
+        {
+            return new Business.BMaquina().Actualizar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Maquina_Eliminar(Dto.Maquina obj)
+        {
+            return new Business.BMaquina().Eliminar(obj);
+        }
+        #endregion
+
+        #region [Gestión Asesores]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Asesor> Asesor_RecuperarFiltros(Dto.Asesor objFiltros)
+        {
+            return new Business.BAsesor().RecuperarFiltrado(objFiltros);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idasesor"></param>
+        /// <returns></returns>
+        public bool Asesor_Insertar(Dto.Asesor obj, byte? idasesor)
+        {
+            bool blnRespuesta = new Business.BAsesor().Crear(obj);
+
+            if (blnRespuesta)
+                idasesor = obj.idasesor;
+            else
+                idasesor = null;
+
+            return blnRespuesta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Asesor_Actualizar(Dto.Asesor obj)
+        {
+            return new Business.BAsesor().Actualizar(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Asesor_Eliminar(Dto.Asesor obj)
+        {
+            return new Business.BAsesor().Eliminar(obj);
+        }
+        #endregion
+    }
+}
---
+++ Tier.Services/ICotizarService.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tier.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICotizarService" in both code and config file together.
+    [ServiceContract]
+    public interface ICotizarService
+    {
+        #region [Gestión Empresas]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        [OperationContract]
+        IEnumerable<Dto.Empresa> Empresa_RecuperarFiltrado(Dto.Empresa objFiltros);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idempresa"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Empresa_Insertar(Dto.Empresa obj, out Nullable<byte> idempresa);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Empresa_Actualizar(Dto.Empresa obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Empresa_Eliminar(Dto.Empresa obj);
+        #endregion
+
+        #region [Gestión Roles]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        [OperationContract]
+        IEnumerable<Dto.Rol> Rol_RecuperarFiltros(Dto.Rol objFiltros);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idrol"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Rol_Insertar(Dto.Rol obj, Nullable<Int16> idrol);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Rol_Actualizar(Dto.Rol obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Rol_Eliminar(Dto.Rol obj);
+        #endregion
+
+        #region [Gestión Listas]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        [OperationContract]
+        IEnumerable<Dto.ItemLista> ItemLista_RecuperarFiltros(Dto.ItemLista objFiltros);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="iditemlista"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool ItemLista_Insertar(Dto.ItemLista obj, out Nullable<int> iditemlista);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool ItemLista_Actualizar(Dto.ItemLista obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool ItemLista_Eliminar(Dto.ItemLista obj);
+        #endregion
+
+        #region [Gestión Usuarios]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        [OperationContract]
+        IEnumerable<Dto.Usuario> Usuario_RecuperarFiltros(Dto.Usuario objFiltros);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idusuario"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Usuario_Insertar(Dto.Usuario obj, Nullable<Int16> idusuario);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Usuario_Actualizar(Dto.Usuario obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Usuario_Eliminar(Dto.Usuario obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Usuario_RestablecerClave(Dto.Usuario obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Usuario_CambiarClave(Dto.Usuario obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Usuario_ValidaNombreUsuario(Dto.Usuario obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        Dto.Sesion Usuario_IniciarSesion(Dto.Usuario obj);
+        #endregion
+
+        #region [Gestión Maquinas]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        [OperationContract]
+        IEnumerable<Dto.Maquina> Maquina_RecuperarFiltros(Dto.Maquina objFiltros);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idmaquina"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Maquina_Insertar(Dto.Maquina obj, Nullable<Int16> idmaquina);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Maquina_Actualizar(Dto.Maquina obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Maquina_Eliminar(Dto.Maquina obj);
+        #endregion
+
+        #region [Gestión Asesores]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        [OperationContract]
+        IEnumerable<Dto.Asesor> Asesor_RecuperarFiltros(Dto.Asesor objFiltros);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idasesor"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Asesor_Insertar(Dto.Asesor obj, Nullable<byte> idasesor);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Asesor_Actualizar(Dto.Asesor obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Asesor_Eliminar(Dto.Asesor obj);
+        #endregion
+    }
+}
