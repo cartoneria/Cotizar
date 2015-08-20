99c3c4ed3807e5ce3521b589f59f2c760e5bb7a7
r-next <r@next-software.co>
2015-08-19 21:05:36 -0500

    Se inserta la referencia del servicio en la capa gui.

---
+++ Tier.Gui/SAL/Empresas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tier.Gui.SAL
+{
+    public class Empresas
+    {
+        void RecuperarEmpresasActivas()
+        {
+        }
+    }
+}
