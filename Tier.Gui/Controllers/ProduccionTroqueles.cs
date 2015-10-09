2171eeaca76ff30642421217eabdb684b3438bf1
r-next <r@next-software.co>
2015-10-08 21:18:24 -0500

    Se agregan las clases parciales para los troqueles, pantones y espectros de producción.

---
+++ Tier.Gui/Controllers/ProduccionTroqueles.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tier.Gui.Controllers
+{
+    public partial class ProduccionController : BaseController
+    {
+    }
+}
