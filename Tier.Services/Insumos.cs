d4cf7d166c0ba2ae570c2b831a4ba4fb0857d10e
r-next <r@next-software.co>
2015-10-18 20:51:08 -0500

    Se programan los métodos del servicio para proveedores y para insumos.

---
+++ Tier.Services/Insumos.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tier.Services
+{
+    public partial class CotizarService : ICotizarService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Insumo> Insumo_RecuperarFiltros(Dto.Insumo objFiltros)
+        {
+            try
+            {
+                return new Business.BInsumo().RecuperarFiltrado(objFiltros);
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idinsumo"></param>
+        /// <returns></returns>
+        public bool Insumo_Insertar(Dto.Insumo obj, out int? idinsumo)
+        {
+            try
+            {
+                bool blnRespuesta = new Business.BInsumo().Crear(obj);
+
+                if (blnRespuesta)
+                    idinsumo = obj.idinsumo;
+                else
+                    idinsumo = null;
+
+                return blnRespuesta;
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Insumo_Actualizar(Dto.Insumo obj)
+        {
+            try
+            {
+                return new Business.BInsumo().Actualizar(obj);
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Insumo_Eliminar(Dto.Insumo obj)
+        {
+            try
+            {
+                return new Business.BInsumo().Eliminar(obj);
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Tier.Services/Proveedores.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tier.Services
+{
+    public partial class CotizarService : ICotizarService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objFiltros"></param>
+        /// <returns></returns>
+        public IEnumerable<Dto.Proveedor> Proveedor_RecuperarFiltros(Dto.Proveedor objFiltros)
+        {
+            try
+            {
+                return new Business.BProveedor().RecuperarFiltrado(objFiltros);
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="idproveedor"></param>
+        /// <returns></returns>
+        public bool Proveedor_Insertar(Dto.Proveedor obj, out int? idproveedor)
+        {
+            try
+            {
+                bool blnRespuesta = new Business.BProveedor().Crear(obj);
+
+                if (blnRespuesta)
+                    idproveedor = obj.idproveedor;
+                else
+                    idproveedor = null;
+
+                return blnRespuesta;
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Proveedor_Actualizar(Dto.Proveedor obj)
+        {
+            try
+            {
+                return new Business.BProveedor().Actualizar(obj);
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Proveedor_Eliminar(Dto.Proveedor obj)
+        {
+            try
+            {
+                return new Business.BProveedor().Eliminar(obj);
+            }
+            catch (Exception ex)
+            {
+                Logs.Error(ex, Logs.ModulosAplicacion.Asesores);
+                throw ex;
+            }
+        }
+    }
+}
