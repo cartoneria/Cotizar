4605d8e3d3a3eda5049e90e709c24110ae389da9
r-next <r@next-software.co>
2015-10-20 22:07:14 -0500

    Se agrega lista de insumos.

---
+++ Tier.Gui/Controllers/ProduccionInsumo.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tier.Gui.Controllers
+{
+    public partial class ProduccionController : BaseController
+    {
+        public ActionResult ListaInsumos()
+        {
+            ViewBag.lstTipoInsumo = SAL.ItemsListas.RecuperarActivosGrupo((byte)Models.Enumeradores.TiposLista.TiposInsumo);
+            return View(SAL.Insimos.RecuperarTodos());
+        }
+
+        public ActionResult CrearInsumo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearInsumo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tier.Gui/SAL/Unsimos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tier.Gui.SAL
+{
+    public static class Insimos
+    {
+        public static IEnumerable<CotizarService.Insumo> RecuperarTodos()
+        {
+            return new clsInsimos().RecuperarTodos();
+        }
+    }
+
+    internal class clsInsimos : BaseServiceAccessParent
+    {
+        internal IEnumerable<CotizarService.Insumo> RecuperarTodos()
+        {
+            objProxy = new CotizarService.CotizarServiceClient();
+            return objProxy.Insumo_RecuperarFiltros(new CotizarService.Insumo());
+        }
+    }
+}
