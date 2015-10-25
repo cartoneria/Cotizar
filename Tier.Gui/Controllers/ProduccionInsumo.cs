5317ae988d5eebb4d4fd6ce60fa2d5ec19a191f9
efrainx3 <efrainr3000@gmail.com>
2015-10-25 09:42:15 -0500

    Se versiona CRUD insumos.

--- Tier.Gui/Controllers/ProduccionInsumo.cs
+++ Tier.Gui/Controllers/ProduccionInsumo.cs
@@ -20,7 +20,7 @@ namespace Tier.Gui.Controllers
         public ActionResult CrearInsumo()
         {
             //Proveedores
-            ViewBag.proveedores_idproveedor = new SelectList(SAL.Proveedores.RecuperarProveedoresActivas(), "idproveedor", "nombre");
+            ViewBag.proveedor_linea_proveedor_idproveedor = new SelectList(SAL.Proveedores.RecuperarProveedoresActivas(), "idproveedor", "nombre");
 
             //ProveedoresLineas
             ViewBag.proveedoreslinea_idproveedorlinea = new SelectList(new List<CotizarService.ProveedorLinea>(), "idlinea", "nombre");
@@ -40,12 +40,61 @@ namespace Tier.Gui.Controllers
         [HttpPost]
         public ActionResult CrearInsumo(CotizarService.Insumo obj)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                int? idinsumo;
+
+                CotizarService.Insumo objInsumo = new CotizarService.Insumo
+                {
+                    activo = obj.activo,
+                    ancho = obj.ancho,
+                    calibre = obj.calibre,
+                    factorrendimiento = obj.factorrendimiento,
+                    itemlista_iditemlista_tipo = obj.itemlista_iditemlista_tipo,
+                    itemlista_iditemlista_unimedcomp = obj.itemlista_iditemlista_unimedcomp,
+                    itemlista_iditemlista_unimedrendi = obj.itemlista_iditemlista_unimedrendi,
+                    observaciones = obj.observaciones,
+                    proveedor_linea_idproveedor_linea = obj.proveedor_linea_idproveedor_linea,
+                    proveedor_linea_proveedor_idproveedor = obj.proveedor_linea_proveedor_idproveedor,
+                    valor = obj.valor
+                };
+
+                CotizarService.CotizarServiceClient _service = new CotizarService.CotizarServiceClient();
+                if (_service.Insumo_Insertar(objInsumo, out idinsumo) && idinsumo != null)
+                {
+                    base.RegistrarNotificación("El insumo fue creado con éxito", Models.Enumeradores.TiposNotificaciones.success, Recursos.TituloNotificacionExitoso);
+                    return RedirectToAction("ListaInsumos", "Produccion");
+                }
+                else
+                {
+                    base.RegistrarNotificación("Falla en el servicio de inserción.", Models.Enumeradores.TiposNotificaciones.error, Recursos.TituloNotificacionError);
+                }
+
+            }
+
+            return View(obj);
         }
 
 
         public ActionResult EditarInsumo(int id)
         {
+            //Proveedores
+            ViewBag.proveedor_linea_proveedor_idproveedor = new SelectList(SAL.Proveedores.RecuperarProveedoresActivas(), "idproveedor", "nombre");
+
+            //ProveedoresLineas
+            ViewBag.proveedoreslinea_idproveedorlinea = new SelectList(new List<CotizarService.ProveedorLinea>(), "idlinea", "nombre");
+
+            //ItemLista Tipo
+            ViewBag.itemlista_iditemlista_tipo = new SelectList(SAL.ItemsListas.RecuperarActivosGrupo((byte)Models.Enumeradores.TiposLista.TiposInsumo), "iditemlista", "nombre");
+
+            //ItemLista UnidadMedida
+            ViewBag.itemlista_iditemlista_unimedcomp = new SelectList(SAL.ItemsListas.RecuperarActivosGrupo((byte)Models.Enumeradores.TiposLista.UnidadesMedida), "iditemlista", "nombre");
+
+            //ItemLista UnidadRendimiento
+            ViewBag.itemlista_iditemlista_unimedrendi = new SelectList(SAL.ItemsListas.RecuperarActivosGrupo((byte)Models.Enumeradores.TiposLista.UnidadesMedida), "iditemlista", "nombre");
+
+
+
             return View();
         }
 
@@ -61,9 +110,23 @@ namespace Tier.Gui.Controllers
             return Json(SAL.Proveedores.RecuperarXId(idProveedor).lineas, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult EliminarInsumo(int id)
+        public ActionResult EliminarInsumo(int idinsumo)
         {
-            return View();
+            try
+            {
+                CotizarService.CotizarServiceClient objService = new CotizarService.CotizarServiceClient();
+                if (objService.Insumo_Eliminar(new CotizarService.Insumo() { idinsumo = idinsumo }))
+                    base.RegistrarNotificación("Se ha eliminado/inactivado el insumo.", Models.Enumeradores.TiposNotificaciones.success, Recursos.TituloNotificacionExitoso);
+                else
+                    base.RegistrarNotificación("El insumo no pudo ser eliminado. Posiblemente se ha inhabilitado.", Models.Enumeradores.TiposNotificaciones.notice, Recursos.TituloNotificacionAdvertencia);
+            }
+            catch (Exception ex)
+            {
+                //Controlar la excepción
+                base.RegistrarNotificación("Falla en el servicio de eliminación.", Models.Enumeradores.TiposNotificaciones.error, Recursos.TituloNotificacionError);
+            }
+
+            return RedirectToAction("ListaInsumos", "Produccion");
         }
 
     }
