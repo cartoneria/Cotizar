2902ddee260d26865a27be541a9d648aa361e7dc
Ricardo Rojas Peña <r@next-software.co>
2015-12-23 10:08:15 -0500

    Programacion cotizar BD
    Se continua con la programacion de bd del cotizar

--- Recursos/Calculos Cotizar.sql
+++ Recursos/Calculos Cotizar.sql
@@ -7,16 +7,15 @@ select
 	-- , tblProd.anchobobina
 	-- , tblProd.cabidalargo
 	-- , tblProd.cabidaancho
-	, tblVPTroq.idVariacion as idVariProdTroq
+	-- , tblVPTroq.idVariacion as idVariProdTroq
 	-- , tblVPTroq.nombre_variacion_produccion
-	, tblMRTroq.idmaquina as idMaqRutaTroq
+	-- , tblMRTroq.idmaquina as idMaqRutaTroq
 	-- , tblMRTroq.nombre
-	, tblVPLito.idVariacion as idVariProdLito
+	-- , tblVPLito.idVariacion as idVariProdLito
 	-- , tblVPLito.nombre_variacion_produccion
-	, tblMRLito.idmaquina as idMaqRutaLito
+	-- , tblMRLito.idmaquina as idMaqRutaLito
 	-- , tblMRLito.nombre
 	, @cantTintas := produccion.ufn_ProdCantTintas(tblProd.idproducto) as cantTintas
-	, @porcTintas := produccion.ufn_ProdPorcTintas(tblProd.idproducto) / 100 as porcTintas
 	-- , tblProd.referenciacliente
 	-- , tblIMReem.idinsumo as idInsuMatReem
 	-- , tblIMR.nombre
@@ -28,18 +27,28 @@ select
 	-- , tblProd.colaminadocabidaancho
     , @cabidaConversion := (tblProd.cabidaancho * tblProd.cabidalargo) as cabidaConversion
 	, @cabidaTroquel := (tblTroq.cabidafibra * tblTroq.cabidacontrafibra) as cabidaTroquel
+    , @areaCartonCaja := (tblProd.largobobina * tblProd.anchobobina) as areaCartonCaja
 	-- , tblIMAcabDer.idinsumo as idInsuMatAcabDer
 	-- , tblProd.anchomaquina_acabadoderecho
 	-- , tblProd.recorrido_acabadoderecho
 	-- , tblIMAcabRev.idinsumo as idInsuMatAcabRev
 	-- , tblProd.anchomaquina_acabadoreverso
 	-- , tblProd.recorrido_acabadoreverso
-	, @factorprecio := (tblProd.factorprecio / 100) as factorprecio
+	, @factorPrecio := (tblProd.factorprecio / 100) as factorPrecio
 	, (tblMRLito.valorplancha * @cantTintas) as costoPlanchas -- esto se debe validar si tiene ya pedidos o si es primera vez del producto
 	, @costoReempaque := ((produccion.ufn_InsumoCostoTotUnidad(tblProd.insumo_idinsumo_reempaque) * 10000) / tblProd.factorrendimientoreempaque) as costoReempaque
 	, @costoAccesorios := produccion.ufn_ProdAccesoriosCostoTotal(tblProd.idproducto) as costoAccesorios
     , @costoFlete := ifnull(((tblProd.largobobina * tblProd.anchobobina) / @cabidaConversion) / ((produccion.ufn_InsumoCostoTotUnidad(@idInsumoFlete) * 10000) / @cabidaTroquel), 0) as costoFlete
     , @costoAcetato := produccion.ufn_ProdAcetatoCostoTotal(tblProd.idproducto) as costoAcetato
+    , @costoCartonCaja := (((@areaCartonCaja/@cabidaConversion) * produccion.ufn_InsumoCostoTotUnidad(tblProd.insumo_idinsumo_material)) / @cabidaTroquel) as costoCarton
+    , @costoCartonColaminado := (((@areaCartonCaja/@cabidaConversion) * produccion.ufn_InsumoCostoTotUnidad(tblProd.insumo_idinsumo_colaminado)) / @cabidaTroquel) as costoColaminado
+    , @costoTintas := produccion.ufn_ProdTintasCostoTotal(tblProd.idproducto) as costoTintas
+    , @costoAcabadoDer := (((tblProd.anchomaquina_acabadoderecho * tblProd.recorrido_acabadoderecho) * produccion.ufn_InsumoCostoTotUnidad(tblProd.insumo_idinsumo_acabadoderecho)) / @cabidaTroquel) as costoAcabadoDer
+    , @costoAcabadoRev := (((tblProd.anchomaquina_acabadoreverso * tblProd.recorrido_acabadoreverso) * produccion.ufn_InsumoCostoTotUnidad(tblProd.insumo_idinsumo_acabadoreverso)) / @cabidaTroquel) as costoAcabadoRev
+    
+    
+    , @costoTotalMaterialUnidad := (@costoReempaque + @costoFlete + @costoAcetato + @costoCartonCaja + @costoCartonColaminado + @costoTintas + @costoAcabadoDer + @costoAcabadoRev) as costoTotalMaterialUnidad
+    , @costototalProcesosUnidad := 0 as costototalProcesosUnidad
 from produccion.producto as tblProd
 	inner join produccion.troquel as tblTroq on tblProd.troquel_idtroquel = tblTroq.idtroquel
 	inner join produccion.insumo as tblIMCaja on tblProd.insumo_idinsumo_material = tblIMCaja.idinsumo
