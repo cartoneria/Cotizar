be823302fd75fcbe48624401d8095101bc1a3e01
efrainx3 <efrainr3000@gmail.com>
2016-04-03 21:11:11 -0500

    Se ajustan validaciones de producto.

--- Tier.Gui/Scripts/Validaciones.js
+++ Tier.Gui/Scripts/Validaciones.js
@@ -54,6 +54,129 @@ var ValProduccion = {
             $("#largomaxmp").rules("remove");
             $('#frmCrearMaquina').validate();
         }
+    },
+    CrearEditarProducto: function() {
+        arrayPantones = JSON.parse($("#hdfEspectro").val());
+
+        if (arrayPantones.length > 1) {
+            $("#pinzalitografica").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#pasadaslitograficas").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#maquinavariprod_idVariacion_rutalitografia").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+        }
+        else {
+            $("#pinzalitografica").rules("remove");
+            $("#pasadaslitograficas").rules("remove");
+            $("#maquinavariprod_idVariacion_rutalitografia").rules("remove");
+            $('#frmCrearProducto').validate();
+            $('#frmEditarProducto').validate();
+        }
+
+        if ($("#insumo_idinsumo_acabadoderecho").val().length > 0) {
+            $("#anchomaquina_acabadoderecho").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#recorrido_acabadoderecho").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+        }
+        else {
+            $("#anchomaquina_acabadoderecho").rules("remove");
+            $("#recorrido_acabadoderecho").rules("remove");
+            $('#frmCrearProducto').validate();
+            $('#frmEditarProducto').validate();
+        }
+
+        if ($("#insumo_idinsumo_acabadoreverso").val().length > 0) {
+            $("#anchomaquina_acabadoreverso").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#recorrido_acabadoreverso").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+        }
+        else {
+            $("#anchomaquina_acabadoreverso").rules("remove");
+            $("#recorrido_acabadoreverso").rules("remove");
+            $('#frmCrearProducto').validate();
+            $('#frmEditarProducto').validate();
+        }
+
+        //insumo_idinsumo_colaminado
+        if ($("#insumo_idinsumo_colaminado").val().length > 0) {
+            $("#insumo_idinsumo_colaminadopegante").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#colaminadoalargo").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#colaminadoancho").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#colaminadocabidalargo").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#colaminadocabidaancho").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+            $("#maquinavariprod_idVariacion_rutacolaminado").rules("add", {
+                required: true,
+                messages: { required: "Requerido" }
+            });
+        }
+        else {
+            $("#insumo_idinsumo_colaminadopegante").rules("remove");
+            $("#colaminadoalargo").rules("remove");
+            $("#colaminadoancho").rules("remove");
+            $("#colaminadocabidalargo").rules("remove");
+            $("#colaminadocabidaancho").rules("remove");
+            $("#maquinavariprod_idVariacion_rutacolaminado").rules("remove");
+            $('#frmCrearProducto').validate();
+            $('#frmEditarProducto').validate();
+        }
+
+        if ($("#insumo_idinsumo_reempaque").val().length > 0 || $("#factorrendimientoreempaque").val().length > 0) {
+            $("#insumo_idinsumo_reempaque").rules("add", {
+                required: true,
+                messages: { required: "Requerido por factor de rendimiento" }
+            });
+            $("#factorrendimientoreempaque").rules("add", {
+                required: true,
+                messages: { required: "Requerido por reempaque" }
+            });
+        }
+        else {
+            $("#insumo_idinsumo_reempaque").rules("remove");
+            $("#factorrendimientoreempaque").rules("remove");
+            $('#frmCrearCliente').validate();
+            $('#frmEditarCliente').validate();
+        }
+
+    },
+    HabilitaCamposColaminado: function (habilitar) {
+        $("#insumo_idinsumo_colaminadopegante").attr("readonly", habilitar);
+        $("#colaminadoalargo").attr("readonly", habilitar);
+        $("#colaminadoancho").attr("readonly", habilitar);
+        $("#colaminadocabidalargo").attr("readonly", habilitar);
+        $("#colaminadocabidaancho").attr("readonly", habilitar);
+        $("#maquinavariprod_idVariacion_rutacolaminado").attr("readonly", habilitar);
     }
 }
 
@@ -62,17 +185,18 @@ var ValComercial = {
         if ($("#identificacion").val().length > 0 || $("#itemlista_iditemlista_tipoidenti").val().length > 0) {
             $("#identificacion").rules("add", {
                 required: true,
-                messages: { required: "Debe ingresar la identificación." }
+                messages: { required: "Debe ingresar la identificación" }
             });
             $("#itemlista_iditemlista_tipoidenti").rules("add", {
                 required: true,
-                messages: { required: "Debe seleccionar un tipo de documento." }
+                messages: { required: "Debe seleccionar un tipo de documento" }
             });
         }
         else {
             $("#identificacion").rules("remove");
             $("#itemlista_iditemlista_tipoidenti").rules("remove");
             $('#frmCrearCliente').validate();
+            $('#frmEditarCliente').validate();
         }
         if ($("#direccion").val().length > 0) {
             $("#municipio_departamento_iddepartamento").rules("add", {
@@ -88,6 +212,7 @@ var ValComercial = {
             $("#municipio_departamento_iddepartamento").rules("remove");
             $("#municipio_idmunicipio").rules("remove");
             $('#frmCrearCliente').validate();
+            $('#frmEditarCliente').validate();
         }
     }
 }
