6c04a057baf1169d3ec238f4a821b4d9f9b3a264
r-netdroix <r@netdroix.com.co>
2016-10-09 11:34:58 -0500

    Se eliminan archivos que ya no son necesarios de los recursos.

--- Recursos/Migraciom/1 CM/08 Accesorios CM.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO produccion.accesorio (activo, fechacreacion, nombre, observaciones, observacionesproduccion, codigo, precio, costomanoobra, empresa_idempresa)
-VALUES (DEFAULT, DEFAULT, 'SOPORTE MANIJA', 'SOPORTE MICRO', NULL, 'SM', 0.0988, 0, 1);
-
-INSERT INTO produccion.accesorio (activo, fechacreacion, nombre, observaciones, observacionesproduccion, codigo, precio, costomanoobra, empresa_idempresa)
-VALUES (DEFAULT, DEFAULT, 'MANIJA', 'Polietileno Negro', NULL, 'M', 100, 10, 1);
-
-INSERT INTO produccion.accesorio (activo, fechacreacion, nombre, observaciones, observacionesproduccion, codigo, precio, costomanoobra, empresa_idempresa)
-VALUES (DEFAULT, DEFAULT, 'CORDON', 'Cordon ', NULL, 'C' , 0.0988, 0, 1);
-
--- Recursos/Migraciom/2 CG/01 Periodos CG.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO seguridad.periodo (nombre, fechainicio, fechafin, vigente, empresa_idempresa, impuestoicacree, porcenfinanciacion, porcenalzageneral, gasto, utilidad)
-VALUES ('Año 2015', '2015-01-01', '2015-12-31', 0, 2, 1.66, 0, 0, 503089759, 250000000);
-
-
-INSERT INTO seguridad.periodo (nombre, fechainicio, fechafin, vigente, empresa_idempresa, impuestoicacree, porcenfinanciacion, porcenalzageneral, gasto, utilidad)
-VALUES ('Año 2016', '2016-01-01', '2016-12-31', 1, 2, 1.66, 0, 0, 503089759, 250000000);
